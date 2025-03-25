fc3522c6949349ab8ee1b08bd510a29917bd10fd
seongjuCho <seongju701@naver.com>
2025-03-25 17:58:25 +0900

    로그인 기능 함수처리

--- mobile/test_mobile_T001.py
+++ mobile/test_mobile_T001.py
@@ -15,15 +15,21 @@ class TestOne:
             driver.find_element(by=AppiumBy.ID,value="com.android.permissioncontroller:id/permission_allow_button").click()
         except Exception as e:
             print("권한 팝업 처리 중 오류 발생 또는 팝업이 표시되지 않음:", e)
-        # 미성년자 앱 접근 확인
-        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.view.View\").instance(5)").click()
-        driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText").click()
-        keycodes = [8, 16, 15, 15]  # '1988 입력
-        for keycode in keycodes:
-            driver.execute_script('mobile: pressKey', {"keycode": keycode})
-        driver.execute_script('mobile: hideKeyboard')
-        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"확인\")").click()
-        sleep(2)
+            # 권한 허용 버튼 클릭
+        try:
+            # 미성년자 앱 접근 확인
+            driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
+                                value="new UiSelector().className(\"android.view.View\").instance(5)").click()
+            driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText").click()
+            keycodes = [8, 16, 15, 15]  # '1988 입력
+            for keycode in keycodes:
+                driver.execute_script('mobile: pressKey', {"keycode": keycode})
+            driver.execute_script('mobile: hideKeyboard')
+            driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"확인\")").click()
+            sleep(2)
+        except Exception as e:
+            print("권한 팝업 처리 중 오류 발생 또는 팝업이 표시되지 않음:", e)
+
     def test_login2(self,driver,login_credentials):
         #비밀번호 오류 팝업 확인
         driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.widget.EditText\").instance(0)").send_keys(login_credentials["ID1"])
@@ -48,3 +54,18 @@ class TestOne:
         driver.activate_app("com.homelearn.schoollearn")
         driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"1\")").click()
 
+    def test_login3(self, driver,login_process,login_credentials):
+        try:
+            driver = login_process(login_credentials["ID1"],login_credentials["PW1"])  # login_process에 ID와 PW 전달하여 로그인 수행
+            print("이건가")
+        except Exception as e:
+            print("권한 팝업 처리 중 오류 발생 또는 팝업이 표시되지 않음:", e)
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"속담의 달인\")").click()
+        driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.Image").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"닫기\").instance(1)").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"날씨\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"날씨\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
+                            value="new UiSelector().className(\"android.widget.Button\").instance(0)").click()
+        driver.find_element(by=AppiumBy.ID, value="com.homelearn.schoollearn:id/txtMessage").click()
+        driver.find_element(by=AppiumBy.ID, value="com.homelearn.schoollearn:id/btnYes").click()
