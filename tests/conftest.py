407b8efb0259c0fa5192c70d9bcddea2542baa4c
seongjuCho <seongju701@naver.com>
2025-03-25 11:01:46 +0900

    브라우저 별 실행 가능하도록 변경

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,39 @@
 from time import sleep
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+
+# 커맨드 라인 옵션 추가
+# 명렁어 pytest --browser_name firefox
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Choose browser: chrome, firefox, edge"
+    )
+
+
 @pytest.fixture(scope="function")
-def setup():
-    driver = webdriver.Chrome()
+def setup(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_name == "edge":
+        driver = webdriver.Edge()
+    else:
+        raise ValueError("해당 브라우저는 지원하지 않습니다")  # 예외 처리 추가
+
     driver.get("https://sem.home-learn.com/sigong/login/loginForm.do")
     driver.maximize_window()
     driver.implicitly_wait(10)
+
     yield driver
     driver.quit()
+
 
 @pytest.fixture(scope="session")
 def login_credentials():
@@ -19,5 +42,3 @@ def login_credentials():
         "adm_pw": "89016410",
         "stu_id": "testplan98"
     }
-
-
