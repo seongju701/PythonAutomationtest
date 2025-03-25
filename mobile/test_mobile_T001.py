8c427f5b2672efd33da5353353df4bdae38302ed
seongjuCho <seongju701@naver.com>
2025-03-25 16:35:31 +0900

    appium 예제 추가

---
+++ mobile/test_mobile_T001.py
@@ -0,0 +1,50 @@
+import pytest
+from time import sleep
+from appium.webdriver.common.appiumby import AppiumBy
+from mobile.conftest import login_credentials
+
+@pytest.mark.usefixtures("driver")
+class TestOne:
+    def test_login1(self, driver):
+        # 권한 허용 버튼 클릭
+        try:
+            # 권한 허용 버튼들이 표시되면 클릭
+            driver.find_element(by=AppiumBy.ID,value="com.homelearn.schoollearn:id/popup_check_permission_btn_confirm").click()
+            driver.find_element(by=AppiumBy.ID,value="com.android.permissioncontroller:id/permission_allow_foreground_only_button").click()
+            driver.find_element(by=AppiumBy.ID,value="com.android.permissioncontroller:id/permission_allow_foreground_only_button").click()
+            driver.find_element(by=AppiumBy.ID,value="com.android.permissioncontroller:id/permission_allow_button").click()
+        except Exception as e:
+            print("권한 팝업 처리 중 오류 발생 또는 팝업이 표시되지 않음:", e)
+        # 미성년자 앱 접근 확인
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.view.View\").instance(5)").click()
+        driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText").click()
+        keycodes = [8, 16, 15, 15]  # '1988 입력
+        for keycode in keycodes:
+            driver.execute_script('mobile: pressKey', {"keycode": keycode})
+        driver.execute_script('mobile: hideKeyboard')
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"확인\")").click()
+        sleep(2)
+    def test_login2(self,driver,login_credentials):
+        #비밀번호 오류 팝업 확인
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.widget.EditText\").instance(0)").send_keys(login_credentials["ID1"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.widget.EditText\").instance(1)").send_keys(login_credentials["PW2"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().text(\"로그인\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().text(\"비밀번호가 맞지 않거나\n등록된 사용자가 아닙니다.\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().text(\"확인\")").click()
+        #정상적으로 로그인 확인
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.widget.EditText\").instance(0)").send_keys(login_credentials["ID1"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().className(\"android.widget.EditText\").instance(1)").send_keys(login_credentials["PW1"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().text(\"로그인\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value="new UiSelector().text(\"1\")").click()
+        #로그아웃
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"닫기버튼\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"로그아웃\")").click()
+        #로그인 유지 상태 로그인
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"android.widget.EditText\").instance(0)").send_keys(login_credentials["ID1"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"android.widget.EditText\").instance(1)").send_keys(login_credentials["PW1"])
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"로그인 유지\")").click()
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"로그인\")").click()
+        driver.terminate_app("com.homelearn.schoollearn")
+        driver.activate_app("com.homelearn.schoollearn")
+        driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"1\")").click()
+
